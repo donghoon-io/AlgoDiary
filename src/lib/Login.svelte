449f1cd47506fbd31a2bee8936c087a399279270
Donghoon Shin <ssshyhy@snu.ac.kr>
2022-07-29 00:25:56 +0900

    Update loading scene

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -30,13 +30,36 @@
 			$nickname = tempNickname;
 		}
 	}
+
+	export let timeout = 1000;
+	export let loadingHidden = false;
+	export let loginHidden = true;
+
+	onMount(() => {
+		setTimeout(() => {
+			loadingHidden = true;
+			loginHidden = false;
+		}, timeout);
+	});
 </script>
 
 <svelte:head>
 	<title>Login - AlgoDiary</title>
 </svelte:head>
 
-<section>
+
+<section class:loadingHidden>
+	<div class="flex items-center justify-center h-screen">
+		<div class="flex items-center justify-center w-1/2 h-2/3 text-center">
+			<div class="text-center">
+				<p class="text-2xl pb-6 mb-6">로딩 중...</p>
+				<div class="loader-1 mx-auto"><span></span></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class:loginHidden>
 	<div class="flex items-center justify-center h-screen">
 		<div class="flex items-center justify-center w-1/2 h-2/3 bg-zinc-100 text-center border border-gray-100 rounded shadow">
 			<div>
@@ -65,5 +88,69 @@
 <style>
 	:global(.notification-content) {
 		font-size: .75rem !important
+	}
+	.loadingHidden {
+		display: none;
+	}
+	.loginHidden {
+		display: none;
+	}
+	.loader-1 {
+		height: 32px;
+		width: 32px;
+		-webkit-animation: loader-1-1 4.8s linear infinite;
+				animation: loader-1-1 4.8s linear infinite;
+	}
+	@-webkit-keyframes loader-1-1 {
+		0%   { -webkit-transform: rotate(0deg); }
+		100% { -webkit-transform: rotate(360deg); }
+	}
+	@keyframes loader-1-1 {
+		0%   { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+	}
+	.loader-1 span {
+		display: block;
+		position: absolute;
+		top: 0; left: 0;
+		bottom: 0; right: 0;
+		margin: auto;
+		height: 32px;
+		width: 32px;
+		clip: rect(0, 32px, 32px, 16px);
+		-webkit-animation: loader-1-2 1.2s linear infinite;
+				animation: loader-1-2 1.2s linear infinite;
+	}
+	@-webkit-keyframes loader-1-2 {
+		0%   { -webkit-transform: rotate(0deg); }
+		100% { -webkit-transform: rotate(220deg); }
+	}
+	@keyframes loader-1-2 {
+		0%   { transform: rotate(0deg); }
+		100% { transform: rotate(220deg); }
+	}
+	.loader-1 span::after {
+		content: "";
+		position: absolute;
+		top: 0; left: 0;
+		bottom: 0; right: 0;
+		margin: auto;
+		height: 32px;
+		width: 32px;
+		clip: rect(0, 32px, 32px, 16px);
+		border: 3px solid #000;
+		border-radius: 50%;
+		-webkit-animation: loader-1-3 1.2s cubic-bezier(0.770, 0.000, 0.175, 1.000) infinite;
+				animation: loader-1-3 1.2s cubic-bezier(0.770, 0.000, 0.175, 1.000) infinite;
+	}
+	@-webkit-keyframes loader-1-3 {
+		0%   { -webkit-transform: rotate(-140deg); }
+		50%  { -webkit-transform: rotate(-160deg); }
+		100% { -webkit-transform: rotate(140deg); }
+	}
+	@keyframes loader-1-3 {
+		0%   { transform: rotate(-140deg); }
+		50%  { transform: rotate(-160deg); }
+		100% { transform: rotate(140deg); }
 	}
 </style>
