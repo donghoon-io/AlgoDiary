2310cb4b7ea28d6a6949a0ebfd5f3c81d88a89b1
Donghoon Shin <ssshyhy@snu.ac.kr>
2022-07-29 00:31:49 +0900

    Update Login.svelte

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -61,9 +61,9 @@
 
 <section class:loginHidden>
 	<div class="flex items-center justify-center h-screen">
-		<div class="flex items-center justify-center w-1/2 h-2/3 bg-zinc-100 text-center border border-gray-100 rounded shadow">
-			<div>
-				<p class="text-sm pb-1">인공지능과 함께 나에대해 알아보기</p>
+		<div class="flex items-center justify-center w-1/2 bg-zinc-100 text-center border border-gray-100 rounded drop-shadow-lg">
+			<div class="py-8 my-8">
+				<p class="text-sm pb-2">인공지능과 함께 나에대해 알아보기</p>
 				<p class="text-4xl font-medium" style="font-family: 'Gentium Book Plus' !important">Algorithm diary</p>
 
 				<form class="w-full max-w-sm mt-6 px-6 pt-6">
